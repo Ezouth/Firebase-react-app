19325428ae99b83e30d6c389b80a5d571f948a9a
Tyler Whidden <Ty.whidden@gmail.com>
2021-06-03 12:14:55 -0400

    Added async fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,9 +1,18 @@
+import React, { useEffect } from 'react';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 
 const AvailableMeals = () => {
+  useEffect(async () => {
+    const fetchMeals = async () => {
+      fetch('https://react-food-app-with-fb-default-rtdb.firebaseio.com/meals.json')
+
+    }
+    fetchMeals();
+  }, []);
+
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
